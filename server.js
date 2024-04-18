d595933390bf376fcdf7ea0ffe93cb8c48b312e3
Quintarius Floyd <quintariusfloyd@Quintariuss-MacBook-Pro.local>
2024-04-18 13:49:28 -0400

    all

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const fs = require('fs');
+const multer = require('multer');
+const cors = require('cors');
 
 const app = express();
+
+// MongoDB URI
 const mongoUri = "mongodb+srv://qfloyd:myR7edfSSZug7AZe@atlascluster.gag1v4a.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"; 
 
-// Define the schema for sneakers
+// MongoDB connection
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Schema definition
 const sneakerSchema = new mongoose.Schema({
   imgSrc: String,
   alt: String,
@@ -15,49 +24,33 @@ const sneakerSchema = new mongoose.Schema({
   description: String
 });
 
-
 const Sneaker = mongoose.model('Sneaker', sneakerSchema);
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB...');
-    initializeSneakerData(); 
-  })
-  .catch(err => console.error('Could not connect to MongoDB...', err));
-
-
+// Initialize sneaker data
 const initializeSneakerData = () => {
   const data = fs.readFileSync('/mnt/data/sneakers', 'utf8');
   const jsonData = JSON.parse(data);
 
-  
   Sneaker.deleteMany({})
-    .then(() => {
-      return Sneaker.insertMany(jsonData.sneakers);
-    })
-    .then(() => {
-      console.log('Sneaker data has been initialized in MongoDB');
-    })
-    .catch(err => {
-      console.error('Error initializing sneaker data in MongoDB', err);
-    });
+    .then(() => Sneaker.insertMany(jsonData.sneakers))
+    .then(() => console.log('Sneaker data has been initialized in MongoDB'))
+    .catch(err => console.error('Error initializing sneaker data in MongoDB', err));
 };
 
+// Middleware
 app.use(express.static(path.join(__dirname, 'final')));
+app.use(express.json());
+app.use(cors());
 
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'final', 'index.html'));
+});
 
 app.get('/api/sneakers', (req, res) => {
   Sneaker.find()
-    .then(sneakers => {
-      res.json({ sneakers });
-    })
-    .catch(err => {
-      res.status(500).send('Error retrieving sneaker data from MongoDB');
-    });
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'final', 'index.html'));
+    .then(sneakers => res.json({ sneakers }))
+    .catch(err => res.status(500).send('Error retrieving sneaker data from MongoDB'));
 });
 
 app.get('/sell', (req, res) => {
@@ -66,8 +59,9 @@ app.get('/sell', (req, res) => {
 
 app.get('/blog', (req, res) => {
   res.sendFile(path.join(__dirname, 'final', 'index.html'));
-})
+});
 
+// Server setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/`);
